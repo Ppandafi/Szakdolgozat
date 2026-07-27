fa9cc83e526d0823ca5e2f9bf5c2021877c5d09d
Ppandafi <balintka676@gmail.com>
2026-07-27 10:29:51 +0200

    Login és regisztrációs logika létrehozása

---
+++ services/auth_service.py
@@ -0,0 +1,46 @@
+from database import SessionLocal, Jatekos
+
+def authenticate_user(email_vagy_nev, jelszo):
+    #Ellenőrzi a felhasználói adatokat az adatbázisban
+    db = SessionLocal()
+    try:
+        felhasznalo = db.query(Jatekos).filter(
+            (Jatekos.email == email_vagy_nev) |
+            (Jatekos.felhasznalonev == email_vagy_nev)
+        ).first()
+        if felhasznalo and felhasznalo.jelszo == jelszo:
+            return True, felhasznalo.email
+        return False, "Helytelen email/felhasználónév vagy jelszó!"
+    except Exception as ex:
+        print(f"Hiba a bejelentkezés során: {ex}")
+        return False, "Hiba az adatbázis kapcsolat során"
+    finally:
+        db.close()
+
+def register_user(email, felhasznalonev, jelszo):
+    #Új felhasználó regisztrálása az adatbázisban
+    db = SessionLocal()
+    try:
+        #Lekérjük, hogy szerepel-e már az adatbázisban ilyen email / felhasználónév
+        felhasznalo = db.query(Jatekos).filter(
+            (Jatekos.email == email) |
+            (Jatekos.felhasznalonev == felhasznalonev)
+        ).first()
+        if felhasznalo:
+            if felhasznalo.email == email:
+                return False, "Ez az email-cím már foglalt!"
+            return False, "Ez a felhasználónév már foglalt!"
+
+        uj_jatekos = Jatekos(
+            felhasznalonev = felhasznalonev,
+            email = email,
+            jelszo = jelszo
+        )
+        db.add(uj_jatekos)
+        db.commit()
+        return True, "Sikeres regisztráció!"
+    except Exception as ex:
+        print(f"Hiba a regisztráció során: {ex}")
+        return False, "Hiba az adatbázis kapcsolat során"
+    finally:
+        db.close()
